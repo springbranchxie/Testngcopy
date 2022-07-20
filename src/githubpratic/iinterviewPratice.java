68b9eebfe81d3bcc1ac12aa02bc606665e5e29af
springbranchxie <xiesj76@gmail.com>
2022-07-20 18:46:41 -0400

    change code again

--- src/githubpratic/iinterviewPratice.java
+++ src/githubpratic/iinterviewPratice.java
@@ -3,7 +3,10 @@ package githubpratic;
 public class iinterviewPratice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	int i=0;
+	int j=1;
+	int result=i+j;
+	System.out.println(result);
 
 	}
 
